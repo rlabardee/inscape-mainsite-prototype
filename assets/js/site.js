a629117a565551865c1fbf88fbcfaecee0d0c205
rjoss <ross@rosslabardee.com>
2018-11-09 17:45:29 -0800

    new components

--- assets/js/site.js
+++ assets/js/site.js
@@ -8,13 +8,19 @@ import 'bootstrap';
 import Header from './components/header';
 import Hero from './components/hero';
 import Narrative from './components/narrative';
+import Carousel from './components/carousel';
+import Figure from './components/figure';
 
 window.jQuery = $;
 $.noConflict();
+
+window.activeColors = [];
 
 // initialize the component classes
 $('.header').each((index, element) => new Header(element));
 $('.hero').each((index, element) => new Hero(element));
 $('.narrative').each((index, element) => new Narrative(element));
+$('.carousel').each((index, element) => new Carousel(element));
+$('.figure').each((index, element) => new Figure(element));
 
 // var draw = SVG('drawing').size('100%', '100%');
