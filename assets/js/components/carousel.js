ac9bf8f9dd7fbf491d227bf4aacc1133efaa5523
rjoss <ross@rosslabardee.com>
2018-11-09 17:44:50 -0800

    updates

---
+++ assets/js/components/carousel.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+
+class Carousel {
+  constructor (element) {
+    this.$element = $(element);
+    console.log('You loaded a carousel!');
+  }
+
+  active () {
+
+  }
+}
+
+export default Carousel;
